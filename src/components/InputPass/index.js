27aab300d15cd9a584a9dca2cd38a7e4d9bd92d7
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-04-22 20:17:25 -0300

    Fix: Required inputs

--- src/components/InputPass/index.js
+++ src/components/InputPass/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const InputPass = ({ children }) => {
     return (
-        <input placeholder={children} className="bg-primary mt-6 mb-6 text-white px-8 py-2 rounded-full transition duration-300 hover:scale-105" type="password" />
+        <input placeholder={children} required className="bg-primary mt-6 mb-6 text-white px-8 py-2 rounded-full transition duration-300 hover:scale-105" type="password" />
     );
 }
 
--- src/components/InputSpace/index.js
+++ src/components/InputSpace/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const InputSpace = ({ children }) => {
     return (
-        <input placeholder={children} className="bg-primary mt-6 mb-6 text-white px-8 py-2 rounded-full transition duration-300 hover:scale-105" />
+        <input placeholder={children} required className="bg-primary mt-6 mb-6 text-white px-8 py-2 rounded-full transition duration-300 hover:scale-105" />
     );
 }
 
